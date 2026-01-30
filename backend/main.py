b2d3ed98672290fdf2991d4208e83ec893afadd4
Ashwin Menon <xmenon.ashwin@tpj.co.jp>
2026-01-30 13:21:08 +0900

    added profile change fuctionalit

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,13 @@ app = FastAPI()
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://ims-omega-eosin.vercel.app",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
